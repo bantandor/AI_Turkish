cc232d9a9a668767691d58975452bc81fe3c2a85
bantandor <30981455+bantandor@users.noreply.github.com>
2020-12-22 05:20:34 +0200

    Create Ödev1_12212020.py

---
+++ Ödev1_12212020.py
@@ -0,0 +1,14 @@
+#İlk Ödev 21 12 2020
+userad = "Taner"
+usersoyad = "Erdoğan"
+useryas = 49
+userboy = 1.85
+userkilo = 90
+print("Adı:{} Soyadı:{}  Yaşı:{}  Boyu:{}cm  Kiloso:{}kg".format(userad, usersoyad, useryas, userboy, userkilo))
+print(f'Adı:{userad} Soyadı:{usersoyad}  Yaşı:{useryas}  Boyu:{userboy}cm  Kilosu:{userkilo}kg')
+
+print(type(userad))
+print(type(usersoyad))
+print(type(useryas))
+print(type(userboy))
+print(type(userkilo))
